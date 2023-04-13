4494f8177153ca3a782936dc221bc1e3509d93c7
Keadon Mitchell <mitchell.keadon@outlook.com>
2023-04-13 05:48:23 -0500

    Add dynamic background that changes with current time of day

--- src/js/weatherComponents.js
+++ src/js/weatherComponents.js
@@ -1,3 +1,5 @@
+import { format, parseISO } from 'date-fns';
+
 export function currentWeatherComponent(data) {
   const wrapper = document.createElement('div');
   wrapper.className = 'current-weather-wrapper';
@@ -8,13 +10,19 @@ export function currentWeatherComponent(data) {
 
   const lastUpdate = document.createElement('p');
   wrapper.appendChild(lastUpdate);
-  lastUpdate.textContent = data.current.last_updated;
+  const time = formatTime12Hour(data.current.last_updated);
+  lastUpdate.textContent = 'Last update' + time;
 
   const currentWeather = buildDataList(data.current);
   wrapper.appendChild(currentWeather);
 
   const currentAstro = buildDataList(data.forecast.forecastday[0].astro);
   wrapper.appendChild(currentAstro);
+
+  const sunrise = currentAstro.querySelector('.sunrise .value').textContent;
+  const sunset = currentAstro.querySelector('.sunset .value').textContent;
+
+  setBackground(time, sunrise, sunset);
 
   return wrapper;
 }
@@ -190,4 +198,72 @@ function formatDataPoint(key, value) {
     default:
       return false;
   }
+}
+
+function setBackground(time, sunrise, sunset) {
+  time = formatTimeToMinutes(formatTime24Hour(time));
+  sunrise = formatTimeToMinutes(formatTime24Hour(sunrise));
+  sunset = formatTimeToMinutes(formatTime24Hour(sunset));
+  let totalDay = sunset - sunrise;
+  let totalNight = 1440 - sunset + sunrise;
+  let midday = sunrise + totalDay / 2;
+  let midnight = (sunset + totalNight / 2) % 1440;
+
+  const intervals = [
+    { start: 0, end: midnight, bg: 'bg-10.png' },
+    { start: midnight, end: midnight + 60, bg: 'bg-11.png' },
+    { start: midnight + 60, end: sunrise - 60, bg: 'bg-12.png' },
+    { start: sunrise - 60, end: sunrise, bg: 'bg-1.png' },
+    { start: sunrise, end: sunrise + 60, bg: 'bg-2.png' },
+    { start: sunrise + 60, end: midday - 90, bg: 'bg-3.png' },
+    { start: midday - 90, end: midday, bg: 'bg-4.png' },
+    { start: midday, end: midday + 90, bg: 'bg-5.png' },
+    { start: midday + 90, end: sunset - 60, bg: 'bg-6.png' },
+    { start: sunset - 60, end: sunset, bg: 'bg-7.png' },
+    { start: sunset, end: sunset + 60, bg: 'bg-8.png' },
+    { start: sunset + 60, end: 1440, bg: 'bg-9.png' },
+  ];
+
+  let backgroundImage;
+
+  for (const interval of intervals) {
+    if (time >= interval.start && time <= interval.end) {
+      backgroundImage = interval.bg;
+      break;
+    }
+  }
+
+  document.querySelector(
+    'body'
+  ).style.backgroundImage = `url(${backgroundImage})`;
+}
+
+function formatTime12Hour(inputTime) {
+  try {
+    const date = parseISO(inputTime);
+    return format(date, 'h:mm aa');
+  } catch {
+    console.warn('invalid date format');
+    return 'Unknown date';
+  }
+}
+
+function formatTime24Hour(inputTime) {
+  const [hoursMinutes, period] = inputTime.split(' ');
+  let [hours, minutes] = hoursMinutes.split(':');
+  hours = parseInt(hours, 10);
+
+  if (period === 'PM' && hours !== 12) {
+    hours += 12;
+  } else if (period === 'AM' && hours === 12) {
+    hours = 0;
+  }
+
+  return `${hours.toString().padStart(2, '0')}${minutes}`;
+}
+
+function formatTimeToMinutes(timeStr) {
+  let hours = parseInt(timeStr.substring(0, 2), 10);
+  let minutes = parseInt(timeStr.substring(2, 4), 10);
+  return hours * 60 + minutes;
 }
